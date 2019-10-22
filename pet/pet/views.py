488890b062dbf024a30867a6da5c25b6b94ac86d
MAYANK KOTHYARI <mayankk@cse.iitb.ac.in>
2019-10-22 17:19:19 +0530

    added django - fixed template

---
+++ pet/pet/views.py
@@ -0,0 +1,8 @@
+from django.http import HttpResponse
+from django.template import loader
+
+
+def index(request):
+    context = {}
+    template = loader.get_template('index.html')
+    return HttpResponse(template.render(context, request))
