d3ce329f6285d886f6d38cade66d3edc2d130ded
Varad Bhatnagar <varadhbhatnagar@cse.iitb.ac.in>
2019-10-30 00:23:38 +0530

    Removed old directory

--- pet/pet/views.py
+++
@@ -1,8 +0,0 @@
-from django.http import HttpResponse
-from django.template import loader
-
-
-def index(request):
-    context = {}
-    template = loader.get_template('index.html')
-    return HttpResponse(template.render(context, request))
